3a2f1d1c7cf68e732f94fadef24e535b166f4089
taeeun.kim <kimtae416@gmail.com>
2022-12-16 21:14:57 +0900

    #Init

---
+++ vue/vue_study/src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" v-model="userId"/>
+        <button @click="myFunction">클릭</button>
+    </div>
+</template>
+<script>
+export default {
+    coponents: {},
+    data() {
+        return {
+            userId: 'taeeun.kim'
+        }
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {
+        myFunction() {
+            // this.userId
+            console.log(this.userId)
+        }
+    }
+}
+</script>
